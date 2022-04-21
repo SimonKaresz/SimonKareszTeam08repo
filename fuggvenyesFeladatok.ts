5bc5118e80db816dcbd046aa30793be67ce49943
SimonKaresz <kareszkaresz0706@gmail.com>
2022-04-21 18:22:56 +0200

    TS függvények feladat

--- fuggvenyesFeladatok.ts
+++ fuggvenyesFeladatok.ts
@@ -1,6 +1,19 @@
+export { };
+
 //Feladatok
 
 function TeglalapTeruletKerulet(a: number, b: number): void {
     document.write(`A téglalap területe: ${a * b}`);
     document.write(`<br>A téglalap kerülete: ${(a + b) * 2}`);
+}
+
+//PárosE függvény
+
+function ParosE(szam: number): boolean {
+    if (szam % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
