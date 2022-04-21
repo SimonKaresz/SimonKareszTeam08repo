2e3fd907fd868d20a799e526d4b09bbe690a8506
SimonKaresz <kareszkaresz0706@gmail.com>
2022-04-21 17:58:41 +0200

    TS függvények feladat

---
+++ fuggvenyesFeladatok.js
@@ -0,0 +1,5 @@
+//Feladatok
+function TeglalapTeruletKerulet(a, b) {
+    document.write("A t\u00E9glalap ter\u00FClete: ".concat(a * b));
+    document.write("<br>A t\u00E9glalap ker\u00FClete: ".concat((a + b) * 2));
+}
---
+++ fuggvenyesFeladatok.ts
@@ -0,0 +1,6 @@
+//Feladatok
+
+function TeglalapTeruletKerulet(a: number, b: number): void {
+    document.write(`A téglalap területe: ${a * b}`);
+    document.write(`<br>A téglalap kerülete: ${(a + b) * 2}`);
+}
