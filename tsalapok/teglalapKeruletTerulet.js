01df4f67572e8f699708a64e0c73632207a36ae7
Simon Károly <94931782+SimonKaresz@users.noreply.github.com>
2022-04-21 23:49:01 +0200

    Delete tsalapok directory

--- tsalapok/teglalapKeruletTerulet.js
+++
@@ -1,6 +0,0 @@
-var oldalA = 10;
-var oldalB = 5;
-var kerulet = (oldalA + oldalB) * 2;
-var terulet = oldalA * oldalB;
-document.write("A téglalap kerülete:" + kerulet + "<br>");
-document.write("A téglalap területe:" + terulet);
--- tsalapok/teglalapKeruletTerulet.ts
+++
@@ -1,8 +0,0 @@
-var oldalA: number = 10;
-var oldalB: number = 5;
-
-var kerulet: number = (oldalA + oldalB) * 2;
-var terulet: number = oldalA * oldalB;
-
-document.write("A téglalap kerülete:" + kerulet + "<br>");
-document.write("A téglalap területe:" + terulet);
