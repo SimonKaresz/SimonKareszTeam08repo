3be1adee2812d4ac4778a040d3f26a7593a1ffe2
SimonKaresz <kareszkaresz0706@gmail.com>
2022-05-05 22:33:33 +0200

    szorgalmi_tablazat

--- Órai munka/2022.05.05/app/feladat/feladat.component.ts
+++ Órai munka/2022.05.05/app/feladat/feladat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dolgozo } from '../minta/minta.component';
 
 @Component({
   selector: 'app-feladat',
@@ -25,6 +26,61 @@ export class FeladatComponent implements OnInit {
 
   aOldal: number = 1;
   bOldal: number = 2;
+
+
+  //Szorgalmi rész
+  interfaceElem: Dolgozo = {
+    nev: "Minta Márton",
+    kor: 22,
+    fizetes: 220000,
+    beosztas: "Követendő példa"
+  }
+
+
+
+  interfaceTomb: Dolgozo[] = [{
+    nev: "Koaxk Ábel",
+    kor: 23,
+    fizetes: 400000,
+    beosztas: "Rendszergazda"
+  },
+  {
+    nev: "Zsíros B. Ödön",
+    kor: 45,
+    fizetes: 1200000,
+    beosztas: "Ügyvezető Igazgató"
+  },
+  {
+    nev: "Meg Győző",
+    kor: 32,
+    fizetes: 600000,
+    beosztas: "Marketing Manager"
+  },
+  {
+    nev: "Békés Csaba",
+    kor: 63,
+    fizetes: 180000,
+    beosztas: "Takarító"
+  },
+  {
+    nev: "Pofá Zoltán",
+    kor: 25,
+    fizetes: 300000,
+    beosztas: "Biztonsági Őr"
+  },
+  {
+    nev: "Fejet Lenke",
+    kor: 22,
+    fizetes: 220000,
+    beosztas: "Irodai Titkár"
+  },
+  {
+    nev: "Vak Cina",
+    kor: 30,
+    fizetes: 500000,
+    beosztas: "Üzem Orvos"
+  }
+  ]
 
 }
 
