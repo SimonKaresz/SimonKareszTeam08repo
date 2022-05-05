6986f4039ea79852690b6b842618203f27fdd2cc
SimonKaresz <kareszkaresz0706@gmail.com>
2022-05-05 19:50:34 +0200

    2022.05.05

--- Órai munka/Angular/DataBindingAndRouting/src/app/app-routing.module.ts
+++ Órai munka/Angular/DataBindingAndRouting/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeladatComponent } from './feladat/feladat.component';
 import { MintaComponent } from './minta/minta.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 const routes: Routes = [
   { path: 'minta', component: MintaComponent },
   { path: 'feladat', component: FeladatComponent },
+  { path: 'pipes', component: PipesComponent },
   { path: 'notfound', component: NotFoundComponent },
   { path: '', redirectTo: '/minta', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
--- Órai munka/Angular/DataBindingAndRouting/src/app/minta/minta.component.ts
+++ Órai munka/Angular/DataBindingAndRouting/src/app/minta/minta.component.ts
@@ -12,6 +12,7 @@ export class MintaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //2022.05.03
   keszito: string = "Simon Károly";
   utvonal: string = "https://angular.io/";
 
@@ -25,4 +26,71 @@ export class MintaComponent implements OnInit {
   }
 
   aOldal: number = 1;
+
+
+  //2022.05.05
+
+  bevasarloLista: string[] = ["alma", "körte", "barack", "szilva", "dió", "mangó", "banán"];
+
+  interfaceElem: Dolgozo = {
+    nev: "Minta Márton",
+    kor: 22,
+    fizetes: 220000,
+    beosztas: "Követendő példa"
+  }
+
+
+
+  interfaceTomb: Dolgozo[] = [{
+    nev: "Koaxk Ábel",
+    kor: 23,
+    fizetes: 400000,
+    beosztas: "Rendszergazda"
+  },
+  {
+    nev: "Zsíros B. Ödön",
+    kor: 45,
+    fizetes: 1200000,
+    beosztas: "Ügyvezető Igazgató"
+  },
+  {
+    nev: "Meg Győző",
+    kor: 32,
+    fizetes: 600000,
+    beosztas: "Marketing Manager"
+  },
+  {
+    nev: "Békés Csaba",
+    kor: 63,
+    fizetes: 180000,
+    beosztas: "Takarító"
+  },
+  {
+    nev: "Pofá Zoltán",
+    kor: 25,
+    fizetes: 300000,
+    beosztas: "Biztonsági Őr"
+  },
+  {
+    nev: "Fejet Lenke",
+    kor: 22,
+    fizetes: 220000,
+    beosztas: "Irodai Titkár"
+  },
+  {
+    nev: "Vak Cina",
+    kor: 30,
+    fizetes: 500000,
+    beosztas: "Üzem Orvos"
+  }//aktuális objektum vége
+  ]//tömb vége
+
+}//class vége
+
+
+export interface Dolgozo {
+  nev: string;
+  kor: number;
+  fizetes: number;
+  beosztas: string;
 }
